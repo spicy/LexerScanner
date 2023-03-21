24566e39be5c98ff90a9d4831f49fb978fb6e830
spicy <spiceycurrey@gmail.com>
2023-03-20 21:21:02 -0700

    Update lexer.py

--- lexer scanner/lexer.py
+++ lexer scanner/lexer.py
@@ -14,59 +14,75 @@
 ###################################################################################################
 ###################################################################################################
 
+import re
+from enum import Enum
+
+class TokenType(Enum):
+    NONE = 0
+    KEYWORD = 1
+    SEPARATOR = 2
+    IDENTIFIER = 3
+    OPERATOR = 4
+    REAL = 5
+
+# instantiate like this: token1 = Token(TokenType.KEYWORD, "while")
+class Token:
+    # constructor
+    def __init__(self, tokenType, str):
+        # Instance variable
+        self.tokenType = tokenType
+        self.str = str
 
 def lexer():
     print ("executing lexer function...")
 
 # program to read file char by char 
-#will put chars together to form a word and will do the same to put a number together 
+# will put chars together to form a word and will do the same to put a number together 
 
+    tokens = []
+    startsWithKeywordRegex = "^while|^if|^int"
+    startsWithOperatorRegex = "^=|^<|^>"
+    startsWithRealRegex = "^[0-9]*" # untested if this captures all
+    startsWithSeparatorRegex = "^(|^)"
 
-    word = '' # this will hold the string 
-    number = '' # this will be a number 
-    current_state = 0
+    with open('input_scode.txt', 'r') as fileobj:
+        # Remove all spaces then start reading from each line of the file
+        filestring = fileobj.read().replace(' ', '')
+        for line in filestring:
+            while True:
+                # continue as long as the line string is not empty
+                if line is "": 
+                    break
+                
+                match = None
+                curTokenType = TokenType.NONE
+                # check if the line starts with any of the supported keywords
+                if (match := re.search(startsWithKeywordRegex, line)) is not None:
+                    # set curTokenType so we later set the token
+                    curTokenType = TokenType.KEYWORD
 
-    with open("input_scode.txt") as fileobj:
-        for line in fileobj:
-             for ch in line:
+                # check if the line starts with any of the supported operators
+                elif (match := re.search(startsWithOperatorRegex, line)) is not None:
+                    # set curTokenType so we later set the token
+                    curTokenType = TokenType.OPERATOR
 
+                # check if the line starts with any of the supported operators
+                elif (match := re.search(startsWithRealRegex, line)) is not None:
+                    # set curTokenType so we later set the token
+                    curTokenType = TokenType.REAL
 
+                # check if the line starts with any of the supported operators
+                elif (match := re.search(startsWithSeparatorRegex, line)) is not None:
+                    # set curTokenType so we later set the token
+                    curTokenType = TokenType.SEPARATOR
 
-                 if ch != ' ' and ch != ';' and ch != '(' and ch != ')' and ch != '<' and ch != '>' and ch != '=': #checks for spaces and ;
-                    word += ch #append to word
+                # something went wrong...
+                else:
+                    print("Error, unable to parse to token")
+                    break
 
-                 if ch.isnumeric() or ch == '.':
-                     number += ch #add into string if it is numeric
-
-                     if current_state == 2:# now we are in 2nd number after the .
-                         current_state = 3
-                         print("real = " + number)
-                         number = ''
-
-                     if current_state == 1: #now we are in 1st number after the .
-                         current_state = 2
-
-                     if ch == '.': #after . set flag to inc number to 1 
-                         current_state += 1
-                         current_state = 1
-
-
-
-                 if ch == ';': #check for ;
-                     print("separator = " + ch) #prints the character (seperator)
-                 if ch == '(':
-                     print("seporator = " + ch)
-                 if ch == ')':
-                     print("seporator = " + ch)
-                 if ch == '<' or ch == '>':
-                     print ("operator = "+ ch)                     
-               
-        
-                 if ch == ' ': #if there is a space word is complete 
-                    if word == "while":
-                        print("keyword = "+ word) #print keyword
-                        word = ''                 #clear the word
-
-                    if word != "while" and word != ' ' and word != '' and ch != ';' and ch != '(' and ch != ')' and ch != '<' and ch != '>' and ch != '=' :
-                        print("identifier = "+ word)
-                        word = '' #clear the word
+                if match is not None and curTokenType is not TokenType.NONE:
+                    # remove whatever matched from the front of the string
+                    line = line[match.len():]
+                    # append the token we found to the running list
+                    tokens.append(Token(curTokenType, match))
